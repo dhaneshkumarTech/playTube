e405eaeda58c60f63282b8ec6de1b6a24098105f
Daani Danish <legit.daani@gmail.com>
2024-05-25 23:05:43 +0500

    mongoDB database configuration

---
+++ src/config/db.config.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+ 
+const connectDb = async()=>{
+    try {
+        const connectionIntance = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MONGODB connected succesfuly to host: ${connectionIntance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED: ", error);
+        exit(1);
+    }
+}
+
+export default connectDb;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+import connectDb from "./config/db.config";
+
+dotenv.config(
+    {
+        path: './env'
+    }
+);
+
+
+const port = process.env.PORT || 3000; 
+
+connectDb()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log(`Server is listening at port ${port}`)
+    })
+})
+.catch((error)=>{
+    console.log(`MongoDb connection failed: ${error}`);
+    throw error;
+})
